e39c15aa336ee8d8780caab0f5cc0518afc467ab
Mark Hahn <mark@hahnca.com>
2021-12-22 18:22:02 -0800

    fixed show.pickup

--- src/App.vue
+++ src/App.vue
@@ -23,95 +23,120 @@ div
             :class="{clsRed: show.IsFavorite, clsDim: !show.IsFavorite}")
       td(style="width:16px;" @click="togglePickUp(show)")
         font-awesome-icon(icon="arrow-down"
-            :class="{clsGrn: show.pickup, clsDim: !show.pickup}")
+            :class="{clsGrn: show.Pickup, clsDim: !show.Pickup}")
 
 </template>
 
 <script>
-import * as emby from "./emby.js"
+import * as emby from "./emby.js";
 
-import { FontAwesomeIcon }      from '@fortawesome/vue-fontawesome'
-import { library }              from '@fortawesome/fontawesome-svg-core'
-import { faHeart, faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon }      from "@fortawesome/vue-fontawesome";
+import { library }              from "@fortawesome/fontawesome-svg-core";
+import { faHeart, faArrowDown } from "@fortawesome/free-solid-svg-icons";
 library.add(faHeart);
 library.add(faArrowDown);
 
 let allShows = [];
 
-const getEmbyUrl = (id) => 
+const getEmbyUrl = (id) =>
   `http://hahnca.com:8096/web/index.html
-   #!/item?id=${id}&serverId=ae3349983dbe45d9aa1d317a7753483e`
-   .replace(/\s*/g, "");
+   #!/item?id=${id}&serverId=ae3349983dbe45d9aa1d317a7753483e`.replace(/\s*/g, "");
 
 export default {
-  name: 'App',
+  name: "App",
 
-  data() { return {
-    shows: [],
-    searchStr: 'great',
-    filterStr: 'No Filter',
-  }},
+  data() {
+    return {
+      shows: [],
+      searchStr: "",
+      filterStr: "No Filter",
+    };
+  },
 
   components: {
-    FontAwesomeIcon
+    FontAwesomeIcon,
   },
 
   methods: {
-
-    select () {(async () => {
-      const srchStr = this.searchStr.toLowerCase();
-      const fltrStr = this.filterStr;
-      this.shows = allShows
-        .filter( show =>
-           (srchStr == '' || show.Name.toLowerCase().includes(srchStr)) && 
-           ( show.IsFavorite || fltrStr != "Favorites")                 &&
-           (!show.IsFavorite || fltrStr != "Not Favorites")             &&
-           ( show.pickup     || fltrStr != "Pickups")                   &&
-           (!show.pickup     || fltrStr != "Not Pickups")               &&
-           ((show.pickup && show.Id[0] == 'p')
-                             || fltrStr != "Pickups/No Emby")
+    select() {
+      (async () => {
+        const srchStr = this.searchStr.toLowerCase();
+        const fltrStr = this.filterStr;
+        this.shows = allShows.filter(
+          (show) =>
+            (srchStr == "" || show.Name.toLowerCase().includes(srchStr)) &&
+            (show.IsFavorite || fltrStr != "Favorites") &&
+            (!show.IsFavorite || fltrStr != "Not Favorites") &&
+            (show.Pickup || fltrStr != "Pickups") &&
+            (!show.Pickup || fltrStr != "Not Pickups") &&
+            (show.Id.startsWith("pkup-") || fltrStr != "Pickups/No Emby")
         );
-    })()},
-
-    showAll () {(async () => {
-      this.searchStr = "";
-      this.filterStr = "No Filter";
-      this.shows = allShows;
-    })()},
-
-    showInEmby (id) {
-      window.open(getEmbyUrl(id), id);
+      })();
     },
 
-    toggleFav (show){(async () => {
-      show.IsFavorite = await emby.toggleFav(show.Id, show.IsFavorite);
-    })()},
+    showAll() {
+      (async () => {
+        this.searchStr = "";
+        this.filterStr = "No Filter";
+        this.shows = allShows;
+      })();
+    },
 
-    togglePickUp (show){(async () => {
-      show.pickup = await emby.togglePickUp(show.Name, show.pickup);
-      // console.log('show.pickup', show.pickup);
-    })()},
+    showInEmby(id) {
+      if (!id.startsWith("pkup-")) window.open(getEmbyUrl(id), id);
+    },
+
+    toggleFav(show) {
+      (async () => {
+        show.IsFavorite = await emby.toggleFav(show.Id, show.IsFavorite);
+        if (show.Id.startsWith("pkup-")) console.log(show);
+      })();
+    },
+
+    togglePickUp(show) {
+      (async () => {
+        show.pickup = await emby.togglePickUp(show.Name, show.pickup);
+        // console.log('show.pickup', show.pickup);
+      })();
+    },
   },
 
-  mounted() { (async() => {
-    await emby.init();
-    allShows = (await emby.loadAllShows(0,5));
-    this.shows = allShows;
-  })()},
-}
+  mounted() {
+    (async () => {
+      await emby.init();
+      allShows = await emby.loadAllShows(0, 5);
+      this.shows = allShows;
+    })();
+  },
+};
 </script>
 
 <style>
-  tr {outline: thin solid;}
-  tr:nth-child(even) {background-color: #f4f4f4;}
-  td {outline: thin solid;}
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;}
-  span {font-size: 48px; color: Dodgerblue}
-  .clsGrn {color:#2f2}
-  .clsRed {color:#f66}
-  .clsDim {color:#ddd}
-
+tr {
+  outline: thin solid;
+}
+tr:nth-child(even) {
+  background-color: #f4f4f4;
+}
+td {
+  outline: thin solid;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+span {
+  font-size: 48px;
+  color: Dodgerblue;
+}
+.clsGrn {
+  color: #2f2;
+}
+.clsRed {
+  color: #f66;
+}
+.clsDim {
+  color: #ddd;
+}
 </style>
